fda25bb1396dcd83bbd4a9229541a884f8e9d37a
Shaik, Javed Basha <Javedbasha.Shaik@CenturyLink.com>
2020-05-21 19:07:29 +0530

    Second Commit

--- Controllers/EMPController.cs
+++ Controllers/EMPController.cs
@@ -22,5 +22,49 @@ namespace Employee.Controllers
             }
         }
 
+
+        [HttpGet("{id}")]
+        public IEnumerable<Employee> EMPGet(int id)
+        {
+            EMPContext dataContext = new EMPContext();            
+            var employees = dataContext.Employee.Where(x => x.employeeId == id);
+            return employees;
+        }
+
+        [HttpPost]
+        public IActionResult EMPPost(Employee emp)
+        {
+            using (EMPContext dataContext = new EMPContext())
+            {
+                dataContext.Employee.Add(emp);                
+                dataContext.SaveChanges();
+            }
+            return CreatedAtAction("Get",emp);
+         }
+
+        [HttpPut]
+        public IActionResult EMPPut(int id, Employee emp)
+        {
+            if(id != emp.employeeId)
+            {
+                return BadRequest();
+            }
+            
+            using (EMPContext dataContext = new EMPContext())
+            {
+                dataContext.SaveChanges();
+
+            }
+            return CreatedAtAction("Get", emp);
+        }
+        [HttpDelete]
+        public IActionResult EMPDelete(int id)
+        {
+            EMPContext datacontext = new EMPContext();
+            var emp_val = datacontext.Employee.FindAsync(id);
+            datacontext.Remove(emp_val);
+            datacontext.SaveChanges();
+            return Ok();
+        }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Employee.Context;
 
 namespace Employee
 {
@@ -26,6 +27,7 @@ namespace Employee
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<EMPContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
